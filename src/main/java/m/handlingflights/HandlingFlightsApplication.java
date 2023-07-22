63e62d218e54dd563e92ad2ab43c7867e9f68565
MaysVerrecas <99118212+MaysVerrecas@users.noreply.github.com>
2023-07-22 23:26:34 +0300

    init

---
+++ src/main/java/m/handlingflights/HandlingFlightsApplication.java
@@ -0,0 +1,13 @@
+package m.handlingflights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandlingFlightsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandlingFlightsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/m/handlingflights/HandlingFlightsApplicationTests.java
@@ -0,0 +1,13 @@
+package m.handlingflights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandlingFlightsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
